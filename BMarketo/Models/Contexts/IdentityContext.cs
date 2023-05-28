dc6160f12e480a779b13300213e9468ae99f8f95
FireCracker6 <111967393+FireCracker6@users.noreply.github.com>
2023-05-28 11:59:37 +0200

    final commit

--- BMarketo/Models/Contexts/IdentityContext.cs
+++ BMarketo/Models/Contexts/IdentityContext.cs
@@ -15,49 +15,5 @@ public class IdentityContext : IdentityDbContext<AppUser>
     public DbSet<UserAddressEntity> AspNetUserAddresses { get; set; }
     public DbSet<NewsletterSubscription> NewsletterSubscriptions { get; set; }
 
-    /*
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-
-
-        var roleId = Guid.NewGuid().ToString();
-        var userId = Guid.NewGuid().ToString();
-
-
-
-        builder.Entity<IdentityRole>().HasData(
-            new IdentityRole
-            {
-                Id = roleId,
-                Name = "admin",
-                NormalizedName = "admin"
-
-            }
-        );
-
-        var passwordHasher = new PasswordHasher<AppUser>();
-
-        builder.Entity<AppUser>().HasData(new AppUser
-        {
-            Id = userId,
-            FirstName = "Jack",
-            LastName = "Russell",
-            UserName = "admin",
-            Email = "administrator@domain.com",
-
-            PasswordHash = passwordHasher.HashPassword(null!, "BytMig123!"),
-
-        });
-
-        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-        {
-            RoleId = roleId,
-            UserId = userId,
-        });
-
-    }
-    */
 }
 
